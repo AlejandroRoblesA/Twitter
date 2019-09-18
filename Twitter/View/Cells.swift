06f713d9514aa013d607bf50460a760df1a32a02
Alejandro <alejandro.rob.alt@gmail.com>
2019-09-18 14:05:11 -0500

    Custom content profile detail

--- Twitter/View/Cells.swift
+++ Twitter/View/Cells.swift
@@ -24,24 +24,65 @@ class UserHeader: DatasourceCell {
 
 class UserCell: DatasourceCell {
     
+    let profileImageView: UIImageView = {
+       let image = UIImageView()
+        image.backgroundColor = .red
+        return image
+    }()
+    
+    
     override var datasourceItem: Any?{
         didSet{
-            nameLabel.text = datasourceItem as? String
+            //nameLabel.text = datasourceItem as? String
         }
     }
     
     let nameLabel: UILabel = {
         let label = UILabel()
         label.text = "TEXT"
+        label.backgroundColor = .green
         return label
+    }()
+    
+    let userNameLabel: UILabel = {
+       let label = UILabel()
+        label.text = "@AlejandroRoblesz"
+        label.backgroundColor = .purple
+        return label
+    }()
+    
+    let bioTextView: UITextView = {
+       let text = UITextView()
+        text.backgroundColor = .yellow
+        return text
+    }()
+    
+    let followButton: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = .cyan
+        return button
     }()
     
     override func setupViews() {
         super.setupViews()
-        backgroundColor = .yellow
         
+        
+        addSubview(profileImageView)
         addSubview(nameLabel)
+        addSubview(userNameLabel)
+        addSubview(bioTextView)
+        addSubview(followButton)
         
-        nameLabel.anchor(topAnchor, left: leftAnchor, bottom: bottomAnchor, right: rightAnchor, topConstant: 0, leftConstant: 0, bottomConstant: 0, rightConstant: 0, widthConstant: 0, heightConstant: 0)
+        profileImageView.anchor(self.topAnchor, left: self.leftAnchor, bottom: nil, right: nil, topConstant: 12, leftConstant: 12, bottomConstant: 0, rightConstant: 0, widthConstant: 50, heightConstant: 50)
+        
+        
+        
+        nameLabel.anchor(profileImageView.topAnchor, left: profileImageView.rightAnchor, bottom: nil, right: followButton.leftAnchor, topConstant: 0, leftConstant: 8, bottomConstant: 0, rightConstant: 12, widthConstant: 0, heightConstant: 20)
+        
+        userNameLabel.anchor(nameLabel.bottomAnchor, left: nameLabel.leftAnchor, bottom: nil, right: nameLabel.rightAnchor, topConstant: 8, leftConstant: 0, bottomConstant: 0, rightConstant: 0, widthConstant: 0, heightConstant: 20)
+        
+        bioTextView.anchor(userNameLabel.bottomAnchor, left: userNameLabel.leftAnchor, bottom: self.bottomAnchor, right: self.rightAnchor, topConstant: 0, leftConstant: 0, bottomConstant: 0, rightConstant: 0, widthConstant: 0, heightConstant: 0)
+        
+        followButton.anchor(topAnchor, left: nil, bottom: nil, right: self.rightAnchor, topConstant: 12, leftConstant: 0, bottomConstant: 0, rightConstant: 12, widthConstant: 120, heightConstant: 34)
     }
 }
