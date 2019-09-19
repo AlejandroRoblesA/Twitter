ef29b6a0e33d9abbf2b09ec5e1cfd3690f882a1c
Alejandro <alejandro.rob.alt@gmail.com>
2019-09-18 23:56:16 -0500

    Model user added

---
+++ Twitter/Model/User.swift
@@ -0,0 +1,15 @@
+//
+//  User.swift
+//  Twitter
+//
+//  Created by Alejandro on 9/18/19.
+//  Copyright © 2019 Alejandro. All rights reserved.
+//
+
+import Foundation
+
+struct User{
+    let name: String
+    let userName: String
+    let bioText: String
+}
